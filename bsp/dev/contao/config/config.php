788b8c818a6a565ee842da87d6674781145757bd
Jerome Wein <florian.krohmer@gmail.com>
2021-03-17 08:29:21 +0100

    splitting bsp from fcb codes

--- bsp/dev/contao/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/** Some extra BE CSS and JS */
-if (TL_MODE == 'BE')
-{
-    $objUser = \BackendUser::getInstance();
-
-    $GLOBALS['TL_CSS'][]        = 'files/layout/styles/css/mz_be.css';
-}
--- bsp/dev/contao/dca/tl_files.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if(TL_MODE === 'BE') 
-{ 
-$GLOBALS['TL_DCA']['tl_files']['fields']['meta']['eval']['metaFields']['copyright'] = '';
-$GLOBALS['TL_DCA']['tl_files']['fields']['meta']['eval']['metaFields']['caption'] = '';
-}
